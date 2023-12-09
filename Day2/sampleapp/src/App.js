805762e841044bce885a47cd60f8e91525a03ce8
VISHWADEEP2004 <vishwakumar0410@gmail.com>
2023-12-09 23:00:47 +0530

    Day2

---
+++ Day2/sampleapp/src/App.js
@@ -0,0 +1,21 @@
+import './Asserts/Css/Style.css'
+import Nav from './Components/Nav'
+function App(){
+    return(
+        <div>
+            <Nav/>
+            <form class="container shadow" onSubmit="register()" >
+        <h3>Registration form</h3>
+        <input type="text" name="" id="names" placeholder="Name" className="name-input"/>
+        <input type="number" name="" id="Age" placeholder="Age" className="name-input"/>
+        <input type="email" name="" id="Email" placeholder="Email" className="name-input"/>
+        <input type="password" name="" id="pass1" placeholder="Password" className="name-input"/>
+        <input type="password" name="" id="pass2" placeholder="Confirm Password" className="name-input"/>
+        <input type="number" name="" id="Mobile Number" placeholder="Phone" className="name-input"/>
+        <input type="submit" value="SUBMIT" className="button-input"/> 
+        </form>
+        </div>
+
+    )
+}
+export default App
---
+++ Day2/sampleapp/src/Components/Nav.js
@@ -0,0 +1,17 @@
+function Nav(){
+    return(
+            <nav class="navbar">
+        <h1 clss="logo">THE WORLD OF CARZ</h1>
+        <ul>
+            <li><a href="day1.html">REGISTER</a></li>
+            <li><a href="login.html">LOGIN</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">HOME</a></li>
+
+        </ul>
+    </nav>
+    
+
+    )
+}
+export default Nav
